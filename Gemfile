f14625159e5bbf6ae8123c05121e5924515e066d
Kevin Curtin <kevincurtin88@gmail.com>
2015-01-15 14:17:59 -0500

    Add s3 sync config

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
+gem 'middleman-s3_sync'
+
 gem "middleman", "~>3.3.0"
 
 # For syntax highlighting
--- config.rb
+++ config.rb
@@ -37,3 +37,19 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'api.contently.com'
+  s3_sync.region                     = 'us-east-1'
+  s3_sync.aws_access_key_id          = ENV['AWS_S3_ID']
+  s3_sync.aws_secret_access_key      = ENV['AWS_S3_SECRET']
+  s3_sync.delete                     = false # We delete stray files by default.
+  # s3_sync.after_build                = false # We do not chain after the build step by default.
+  # s3_sync.prefer_gzip                = true
+  # s3_sync.path_style                 = true
+  # s3_sync.reduced_redundancy_storage = false
+  # s3_sync.acl                        = 'public-read'
+  # s3_sync.encryption                 = false
+  # s3_sync.prefix                     = ''
+  # s3_sync.version_bucket             = false
+end
