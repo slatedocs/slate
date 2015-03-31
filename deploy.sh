a1db5739d4fe0ea0004078a83fe1e8d45d7ae301
Joseph Misiti <joseph.misiti@gmail.com>
2015-03-31 15:58:34 -0400

    added + updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,21 @@
 
 source $(dirname $0)/settings.sh
 
+echo "building docs ..."
+rake
+echo "done."
+
 echo "compressing assets ..."
-tar cvzf source.tar.gz source/
-echo "done ..."
+tar cvzf build.tar.gz build/
+echo "done ."
 
 echo ""
 
 echo "copying assets to ec2 ..."
 scp source.tar.gz $server:
-echo "done"
+echo "done."
 
-ssh $server 'tar xvf source.tar.gz'
-ssh $server 'rm -f source.tar.gz'
+echo "deloying & unpacking ..."
+ssh $server 'tar xvf build.tar.gz'
+ssh $server 'rm -f build.tar.gz'
+echo "done."
