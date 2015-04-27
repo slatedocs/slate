13a43e7f7e7c27826820e4183b6ca6d52523a456
Akil Burgess <akilburgess@gmail.com>
2015-04-27 10:24:58 +0100

    Fixed issue #7 with the cURL examples
    - Fixed an issue where the cURL examples had an invalid Authorization
    Header

--- helpers/request_helpers.rb
+++ helpers/request_helpers.rb
@@ -5,7 +5,7 @@ module RequestHelpers
   def curl(method, url)
     code = %(curl -X #{method} "#{url}"\n) <<
            %(  -H "User-Agent: MyAwesomeApp"\n) <<
-           %(  -H "Authorization: Bearer: #{ResourceHelpers::CLIENT_CREDENTIALS_TOKEN["access_token"]}")
+           %(  -H "Authorization: Bearer #{ResourceHelpers::CLIENT_CREDENTIALS_TOKEN["access_token"]}")
     write_language(code, "shell")
   end
 
