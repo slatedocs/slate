fca975a2c0bd937c0c5bf0ca72980a0f63be3fcd
Hendrik Rust <hendrik@dcinteractive.co.za>
2015-10-06 12:24:25 +0200

    feat: added live reload

--- config.rb
+++ config.rb
@@ -28,6 +28,9 @@ end
 activate :relative_assets
 set :relative_links, true
 
+# Livereload
+activate :livereload, :host => "127.0.0.1"
+
 # Build Configuration
 configure :build do
   activate :minify_css
