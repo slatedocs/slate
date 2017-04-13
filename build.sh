0a4dfb82dca7ba727a00e5b59978b437829f9436
Neal Richardson <neal@crunch.io>
2017-04-13 08:53:57 -0700

    Print some stuff

--- build.sh
+++ build.sh
@@ -8,9 +8,12 @@ function main {
         exit 0
     fi
 
+    echo "Building..."
     rake build
+    echo "Checking out gh-pages..."
     git checkout gh-pages
     git reset --hard
+    echo "Moving built artifiacts..."
     rm -rf fonts
     rm -rf javascripts
     rm -rf images
@@ -23,7 +26,9 @@ function main {
     git add stylesheets
     git add examples
     git add index.html
+    echo "Committing them..."
     git commit -m "Updating gh-pages"
+    echo "Push!"
     git push
 }
 
