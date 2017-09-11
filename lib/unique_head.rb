25690c1469c63b6f88473c8dcede2220d02786e9
greenhost87 <greenhost87@gmail.com>
2017-09-12 00:49:47 +0300

    Solution for https://github.com/lord/slate/issues/793

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,17 +1,28 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+
+$head_count = {}
+
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
-    @head_count = {}
   end
+
   def header(text, header_level)
     friendly_text = text.parameterize
-    @head_count[friendly_text] ||= 0
-    @head_count[friendly_text] += 1
-    if @head_count[friendly_text] > 1
-      friendly_text += "-#{@head_count[friendly_text]}"
+    $head_count[friendly_text] = header_level
+
+    last_saw_level = header_level
+
+    if header_level > 1
+      $head_count.reverse_each { |text, current_level|
+        if last_saw_level >= 1
+          friendly_text.prepend("#{text}-") if current_level < header_level && current_level < last_saw_level
+          last_saw_level = current_level if current_level < last_saw_level
+        end
+      }
     end
+
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
   end
 end
