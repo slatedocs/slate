ba96efcfb7565f98968991b2720e7ea15090fa56
greenhost87 <greenhost87@gmail.com>
2017-09-18 15:25:43 +0300

    Major bug fix, code simplification

--- lib/nesting_unique_head.rb
+++ lib/nesting_unique_head.rb
@@ -1,7 +1,7 @@
-# Unique header generation
+# Nested unique header generation
 require 'middleman-core/renderers/redcarpet'
 
-$head_count = {}
+$headers_history = {}
 
 class NestingUniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
@@ -10,17 +10,12 @@ class NestingUniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
 
   def header(text, header_level)
     friendly_text = text.parameterize
-    $head_count[friendly_text] = header_level
-
-    last_saw_level = header_level
+    $headers_history[header_level] = text.parameterize
 
     if header_level > 1
-      $head_count.reverse_each { |text, current_level|
-        if last_saw_level >= 1
-          friendly_text.prepend("#{text}-") if current_level < header_level && current_level < last_saw_level
-          last_saw_level = current_level if current_level < last_saw_level
-        end
-      }
+      for i in (header_level - 1).downto(1)
+        friendly_text.prepend("#{$headers_history[i]}-") if $headers_history.key?(i)
+      end
     end
 
     return "<h#{header_level} id='#{friendly_text}'>#{text}</h#{header_level}>"
