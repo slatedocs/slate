31b6deed115462f014be78fa50e1a864b9e349c0
Tejas Dinkar <tejas@gja.in>
2017-11-07 23:09:27 +0530

    Build Script

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,13 @@
 #! /bin/sh -e
 
+REPO_NAME="$1"
+TAG_NAME="$2"
+
+FULL_IMAGE="$REPO_NAME:$TAG_NAME"
+
+echo $TAG_NAME > image-name
 bundle install
 bundle exec middleman build
-docker build -t quay.io/quintype/documentation .
+docker build . -t $FULL_IMAGE
+docker push $FULL_IMAGE
+docker rmi $FULL_IMAGE
