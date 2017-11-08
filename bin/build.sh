1162c7b7927f43c42245953f29f3ed9bf67f8092
Tejas Dinkar <tejas@gja.in>
2017-11-08 07:50:00 +0530

    Adding build and docker build

--- bin/build.sh
+++ bin/build.sh
@@ -1,13 +1,11 @@
 #! /bin/sh -e
 
+# Move this bit into trojan rabbit
 REPO_NAME="$1"
-TAG_NAME="$2"
-
+TAG_NAME="$GO_PIPELINE_NAME-$GO_PIPELINE_COUNTER-$GO_STAGE_NAME-$GO_STAGE_COUNTER"
 FULL_IMAGE="$REPO_NAME:$TAG_NAME"
 
+./bin/docker-build.sh $FULL_IMAGE
 echo $TAG_NAME > image-name
-bundle install
-bundle exec middleman build
-docker build . -t $FULL_IMAGE
 docker push $FULL_IMAGE
 docker rmi $FULL_IMAGE
