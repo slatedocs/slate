b61db7688ea19747a89405bdca98628e0692c5f4
KMcB <cavrois.cedric@hotmail.fr>
2018-06-27 14:46:37 +0200

    build doc script

---
+++ make.sh
@@ -0,0 +1,19 @@
+
+rm -rf build mjml mjml-doc || true &&
+
+echo 'Cloning the repos'
+git clone https://github.com/mjmlio/mjml mjml &&
+rm -rf source/index.html.md &&
+
+
+echo 'Building documentation...' &&
+npm install &&
+npm run concat-docs-md &&
+
+mv js/CONTENT.md source/index.html.md &&
+
+bundle install &&
+bundle exec middleman build &&
+mv build ./mjml-doc &&
+rm -rf mjml node_modules &&
+echo 'Everything has been done master'
