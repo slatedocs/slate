39cd3d571e5b88c06f7826bbd3f904d848e7397d
KMcB <cavrois.cedric@hotmail.fr>
2018-06-28 12:43:37 +0200

    move static doc from mjml repo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 import fs from 'fs'
 import path from 'path'
-import config from './config.json'
+import config from '../static-doc/config.json'
 
 const DEST = './CONTENT.md'
 const docFilesPath = '../'
