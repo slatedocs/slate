535dcd7e2e84053bcbfbee190f6582060d2bc023
nkohen <nadavk25@gmail.com>
2018-08-02 15:09:08 -0500

    Fixed bug in push

--- DanPush.sh
+++ DanPush.sh
@@ -5,8 +5,9 @@ if [[ $1 ]]; then
 	suredbits_web_dir=~/IdeaProjects/SuredBits-Web/
 
 	cd "$suredbits_web_dir"
+	git checkout new_slate
 	git commit -am "$1"
-	git push gitlab-master new_slate
+	git push gitlab new_slate
 else
 	echo "Don't forget a commit message! Try again."
 fi
