cc30a15d985ac862f374b8c8f4631e8fbdbf69c5
KMcB <cavrois.cedric@hotmail.fr>
2019-01-02 18:29:08 +0100

    use mjml repo static doc

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 import fs from 'fs'
 import path from 'path'
-import config from '../static-doc/config.json'
+import config from '../mjml/doc/config.json'
 
 const DEST = './CONTENT.md'
 const docFilesPath = '../'
