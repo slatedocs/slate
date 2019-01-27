0ffe7360ef8d7a77e24ff70ed105dc64c3d69a8a
Yaniv Iny <yaniv@practitest.com>
2019-01-27 14:18:39 +0200

    updated deploy method

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 bundle exec middleman build --clean
-aws s3 sync build/index.html  s3://pt_public/api/slate-apiv2/index.html --acl public-read
-aws cloudfront create-invalidation --distribution-id E2V3PVGRJTV0N8 --paths /api/slate-apiv2/index.html
+aws s3 cp build/index.html s3://pt_public/api/slate-apiv2/index.html --acl public-read
+aws cloudfront create-invalidation --distribution-id E2V3PVGRJTV0N8 --paths /api/slate-apiv2/index.html | grep Id
+
+# aws cloudfront get-invalidation --id the_id --distribution-id E2V3PVGRJTV0N8 |grep Status
