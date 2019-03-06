8ced5f1218048097d432c12f2379fb35174405e8
Torkel Rogstad <torkel@rogstad.io>
2019-03-06 15:37:52 +0100

    Add WS playground button

--- .push-to-wp.sh
+++ .push-to-wp.sh
@@ -7,7 +7,7 @@ fi
 IP=$1
 
 echo "Transferring build folder to server..."
-scp -r build ubuntu@$IP:/home/ubuntu
+scp -r build ubuntu@$IP:/home/ubuntu/api-build
 
 echo "Moving build folder to WordPress..."
 ssh ubuntu@$IP "./update-wp.sh"
