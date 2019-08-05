658e4085ca911295a9c3792ca330d09f56ffe436
Ross Grambo <rossgrambo@asana.com>
2019-08-05 11:28:30 -0700

    Fixed circle ci script, increased load speed by hiding code examples by default

--- lib/multilang.rb
+++ lib/multilang.rb
@@ -12,7 +12,12 @@ module Multilang
       parts = full_lang_name.split('--')
       rouge_lang_name = (parts) ? parts[0] : "" # just parts[0] here causes null ref exception when no language specified
       super(code, rouge_lang_name).sub("highlight #{rouge_lang_name}") do |match|
-        match + tab_class + full_lang_name
+        extra = ''
+        tab_class_lang = tab_class + full_lang_name
+        if tab_class_lang == ' tab-javascript--nodejs' || tab_class_lang == ' tab-ruby' || tab_class_lang == ' tab-java' || tab_class_lang == ' tab-python' || tab_class_lang == ' tab-php'
+          extra = '" style="display:none'
+        end
+        match + tab_class_lang + extra
       end
     else
       super(code, full_lang_name)
