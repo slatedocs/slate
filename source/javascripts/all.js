679539b0a1989dcbdba53008139505b96507ed76
Alejandro López <alejandrolopez.gd@gmail.com>
2021-03-15 09:02:53 -0300

    ✨ feat(params): allow expand/collapse (#26)
    * ✨ feat(endpoints): allow expanding/collapsing params
    
    * 🔧 fix(docs): update dom
    
    * ✏️ fix(actions): fix deploy
    
    Some ES features are not supported
    
    Co-authored-by: Alejandro <alejandro@admetricks.com>

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,4 +2,5 @@
 //= require ./app/_search
 //= require ./app/_search
 //= require ./app/_go-to-top
+//= require ./app/_expand-params
 //= require ./lib/_page-text-replace.min
---
+++ source/javascripts/app/_expand-params.js
@@ -0,0 +1,29 @@
+function onParamToggle(event) {
+  const target = event.target;
+  const param = target.parentElement.parentElement;
+
+  target.classList.toggle("active");
+  toggleVisibility(param.nextElementSibling);
+}
+
+function toggleVisibility(el) {
+  const isParentLevel = el.classList.contains("param__child1");
+
+
+  if (isParentLevel) {
+    // next parent. they're always visible
+    return;
+  }
+
+  console.log("Toggle sibling:", el);
+  el.classList.toggle("param__child__active");
+  toggleVisibility(el.nextElementSibling);
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  const toggles = document.querySelectorAll("button.param__toggle");
+
+  Array.from(toggles).forEach(function (toggle) {
+    toggle.addEventListener("click", onParamToggle);
+  });
+});
