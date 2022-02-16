cc72dc8de38c0f1c3df8f70aefe61ea9a193ced3
Matt Matthias <mmatthias@evoraglobal.com>
2022-02-16 20:29:10 +0000

    Added markdown.erb

--- config.rb
+++ config.rb
@@ -60,4 +60,7 @@ set :port, 4567
 
 helpers do
   require './lib/toc_data.rb'
+  require './lib/markdown_erb.rb'
 end
+
+helpers MarkdownErb
---
+++ lib/markdown_erb.rb
@@ -0,0 +1,19 @@
+module MarkdownErb
+    def markdown(md_text)
+      md_renderer.render(md_text)
+    end
+  
+    def md_renderer
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    end
+  
+    def dictionary_description(field)
+      desc = data.dictionary.descriptions[field]
+      link = data.dictionary.links[field]
+      if link
+        markdown("#{desc} See #{link} for further information.")
+      else
+        desc
+      end
+    end
+  end
