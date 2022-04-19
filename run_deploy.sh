3417db34d3913d5a94cb71e7077789b38fa30ca9
Moises De La Cruz <moisesdelacruz.dev@gmail.com>
2022-04-19 18:26:51 -0500

    TAUR-4130 | Upgrade NGINX ingress configuration for all repositories.
    On branch TAUR-4130
    
    modified:   .production/ingress/master.yaml
    modified:   .production/ingress/staging.yaml
    modified:   README.md
    modified:   run_deploy.sh
    modified:   run_pre_deploy.sh

--- run_deploy.sh
+++ run_deploy.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $CIRCLE_BRANCH == "staging" ]]; then
-  RANCHER_CLUSTER=$RANCHER_CLUSTER_STAGING
-fi;
-
-# rancher login
-echo -e "${yellow}\nRancher Login${noc}"
-rancher login $RANCHER_API --token $RANCHER_TOKEN --context $RANCHER_CLUSTER
-
 # Set image to deployments files
 sed -i -e "s/%{{BRANCH}}/$CIRCLE_BRANCH-$CIRCLE_BUILD_NUM/g" .production/deployments/deployment.yaml
 
 # kubernetes apply default deployment
 echo -e "${blue}\nDefault Deploy${noc}"
-rancher kubectl apply \
+kubectl apply \
   -f .production/deployments/deployment.yaml \
   -f .production/services/web-service.yaml \
   -f .production/ingress/$CIRCLE_BRANCH.yaml \
